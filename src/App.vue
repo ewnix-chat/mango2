c42bc9c8e692773679a0bd7484e506ba0c45c8f3
Kevin Thompson <phlux@ewnix.net>
2023-08-19 15:29:59 -0500

    Fresh vite/vue init.

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <div class="sidebar">
+      <h2>Channels</h2>
+      <ul>
+        <li>#general</li>
+        <li>#help</li>
+        <li>#random</li>
+      </ul>
+    </div>
+    <div class="main">
+      <div class="header">
+        <h2>#general</h2>
+      </div>
+      <div class="chat">
+        <div class="message">User1: Hello, world!</div>
+        <div class="message">User2: Hi, User1!</div>
+      </div>
+      <div class="input">
+        <input type="text" placeholder="Type your message..."/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {};
+  }
+}
+</script>
+
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #2c2c2c;
+    color: #ffffff;
+  }
+  .container {
+    display: flex;
+    height: 100vh;
+  }
+  .sidebar {
+    width: 20%;
+    padding: 20px;
+    background-color: #353535;
+  }
+  .main {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+  }
+  .header {
+    padding: 20px;
+    background-color: #3a3a3a;
+  }
+  .chat {
+    flex: 1;
+    padding: 20px;
+    overflow-y: scroll;
+  }
+  .message {
+    margin-bottom: 10px;
+  }
+  .input {
+    padding: 20px;
+    background-color: #3a3a3a;
+  }
+  input {
+    width: 100%;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+  }
+</style>
+
