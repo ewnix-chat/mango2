e4d12ff7a69f485c0d93f8e4e540a0a2b382a5e6
Kevin Thompson <phlux@ewnix.net>
2023-08-20 06:06:40 -0500

    Compelte CSS rewrite.

--- src/App.vue
+++ src/App.vue
@@ -43,163 +43,176 @@ export default {
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap');
 
-  body, html {
-    font-family: 'Inter', sans-serif;
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 100%;
-  }
-  .container {
-    display: flex;
-    height: 100vh;
-  }
-  .login-inputs {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
-    width: 80%;
-  }
-  h2 {
-    font-weight: 600;
-    margin-bottom: 20px;
-  }
-  input {
-    width: 100%;
-    padding: 15px;
-    margin-bottom: 10px;
-    border: none;
-    border-radius: 12px;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
-  }
-  button {
-    padding: 15px 30px;
-    border: none;
-    border-radius: 12px;
-    color: #ffffff;
-    cursor: pointer;
-    font-weight: 600;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
-    margin-top: 10px;
-    background-color: #007aff;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body, html {
+  font-family: 'Inter', sans-serif;
+  width: 100%;
+  height: 100%;
+}
+
+.container {
+  display: flex;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.sidebar {
+  width: 20%;
+  height: 100vh;
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-inputs {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  width: 80%;
+}
+
+h2 {
+  font-weight: 600;
+  margin-bottom: 20px;
+}
+
+input {
+  width: 100%;
+  padding: 15px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 12px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+}
+
+button {
+  padding: 15px 30px;
+  border: none;
+  border-radius: 12px;
+  color: #ffffff;
+  cursor: pointer;
+  font-weight: 600;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+  margin-top: 10px;
+  background-color: #007aff;
+}
+
+.main {
+  background-color: #ffffff;
+  width: 80%;
+  height: 100vh;
+}
 
 /* Light theme styles */
-  body, .sidebar {
-    background-color: #ffffff;
-    color: #333333;
-  }
+body, .sidebar {
+  background-color: #ffffff;
+  color: #333333;
+}
 
-  input {
-    background-color: #f2f2f2;
-    color: #333333;
-  }
+input {
+  background-color: #f2f2f2;
+  color: #333333;
+}
 
 /* Dark theme styles */
-  .dark body, .dark .sidebar {
-    background-color: #333333;
-    color: #ffffff;
-  }
-  
-  .dark input {
-    background-color: #4a4a4a;
-    color: #ffffff;
-  }
+.dark body, .dark .sidebar {
+  background-color: #333333;
+  color: #ffffff;
+}
 
-  .main {
-    background-color: #ffffff;
-    width: 80%;
+.dark input {
+  background-color: #4a4a4a;
+  color: #ffffff;
+}
+
+.dark .main {
+  background-color: #333333;
+}
+
+/* Toggle Switch */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+  margin-top: 15px;
+}
+
+.switch input {
+  display: none;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #007aff;
+}
+
+input:checked + .slider:before {
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+/* Mobile */
+@media (max-width: 768px) {
+  .container {
+    flex-direction: column;
+    align-items: center;
     height: 100vh;
-  }
-
-  .dark .main {
-    background-color: #333333;
-  }
-
-  /* Toggle Switch */
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-    margin-top: 15px;
-  }
-  .switch input {
-    display: none;
-  }
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    transition: 0.4s;
-  }
-  .slider:before {
-    position: absolute;
-    content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    transition: 0.4s;
-  }
-  input:checked + .slider {
-    background-color: #007aff;
-  }
-  input:checked + .slider:before {
-    transform: translateX(26px);
-  }
-  .slider.round {
-    border-radius: 34px;
-  }
-  .slider.round:before {
-    border-radius: 50%;
   }
 
   .sidebar {
-    width: 20%;
+    width: 100%;
     height: 100vh;
-    padding: 1em;
-    box-sizing: border-box;
+    padding: 0;
+    align-items: center;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
   }
 
-/* Mobile */
-
-  @media (max-width: 768px) {
-    .container {
-      flex-direction: column;
-      align-items: center;
-      height: 100vh;
-      width: 100%;
-      border: none;
-      padding: 0;
-      margin: 0;
-    }
-    .sidebar {
-      width: 100%;
-      height: 100vh;
-      padding: 0;
-      align-items: center;
-      border: none;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-    }
-    .login-inputs {
-      width: 90%;
-    }
-    .theme-switch {
-      justify-content: center;
-    }
+  .login-inputs {
+    width: 90%;
   }
+
+  .theme-switch {
+    justify-content: center;
+  }
+}
 </style>
